2683ad8c6033ed014bb07b05078c86730bbe5846
danielem98 <marini.daniele98@gmail.com>
2021-06-03 17:32:46 +0200

    InputTest corretto

--- src/ires/corso/Main.java
+++ src/ires/corso/Main.java
@@ -20,7 +20,7 @@ public class Main {
                 System.out.println("Il numero maggiore e: " + n1);
             }
             else{
-                System.out.println("Il numero maggiore e: " + n1);
+                System.out.println("Il numero maggiore e: " + n3);
             }
         }
         else{
